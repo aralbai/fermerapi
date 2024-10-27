8190154d4621f6eae7e523957f41cb00ba6ed6fe
xabibullayev <xabibullayevmm@gmail.com>
2024-10-27 21:09:59 +0500

    finished

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(express.static("/Images"));
-app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/Images", express.static(path.join(__dirname, "Images")));
 
 mongoose
   .connect(
